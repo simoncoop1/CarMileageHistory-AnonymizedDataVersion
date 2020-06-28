1af12332f66821aa6478e1f252d8319099c699be
Simon Cooper <30930833+simoncoop1@users.noreply.github.com>
2020-06-28 18:10:35 +0100

    de-anonymise method

--- generator.py
+++ generator.py
@@ -2,6 +2,7 @@ import os
 import csv
 import argparse
 import sys
+import plot
 
 ##iterate all records in the data
 def dGenerateCars():
@@ -47,7 +48,8 @@ def plotit(car_id):
     for c in dGenerateCars():
 
         if c[1]==theid:
-            thelist.append(c)
+            thelist.append(c)   
+
 
     thelist =sorted(thelist, key=lambda r: r[2])
     print(thelist)
@@ -55,9 +57,20 @@ def plotit(car_id):
     milages = []
     dates = []
     for a in thelist:
-        milages.append(a[6])
+        milages.append(int(a[6]))
         dates.append(a[2])
 
+    plot.plotIt(dates,milages)
+
+#your car of interest needs to be de-anonymised before anything interesting can be done
+#this is done but taking key pieces of information about the history of your car. This
+#can be obtained from https://www.gov.uk/check-mot-history and entering you registration
+# number.
+def GetCarID(first_registered,amot_date,amot_mileage):
+    for c in dGenerateCars():
+        if c[13]==first_registered and  c[2]==amot_date and c[6]==str(amot_mileage):
+            print("the carid is: " + c[1])
+            return c[1]
 
 
 if __name__ == "__main__":
@@ -81,14 +94,18 @@ if __name__ == "__main__":
         plotit(theid)
         sys.exit()
 
-    for c in dGenerateCars():
+    #for c in dGenerateCars():
         #if len(c)<2:
         #    print("??")
         #    print(c)
         #    continue
-        #if c[1]=='591469624':
-        if c[1]==theid:
-            print(c)
+    #    if c[1]=='1103092882':
+    #        print(c)
+
+
+    theid = GetCarID("2007-05-01","2017-04-04","69646")
+    plotit(theid)
+       
 
     print(help)
         
