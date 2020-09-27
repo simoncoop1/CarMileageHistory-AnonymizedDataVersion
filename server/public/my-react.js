1f6860a876b3ee429da447d38f878a174db3d7c5
Simon Cooper <30930833+simoncoop1@users.noreply.github.com>
2020-09-27 21:42:36 +0100

    more static code

--- server/public/my-react.js
+++ server/public/my-react.js
@@ -13,7 +13,15 @@ class SingleItem extends React.Component{
         super(props);
     }
     render(){
-        return (<tr><td>a make</td><td>a model</td><td>mean age</td>mean mileage<td></td></tr>);
+        const ITEM = ["a make","a model","a mean age","a mean mileage" ];
+        return (
+            <tr>
+            <td>{ITEM[0]}</td>
+            <td>{ITEM[1]}</td>
+            <td>{ITEM[2]}</td>
+            <td>{ITEM[3]}</td>
+            </tr>
+        );
     }
 }
 
@@ -23,8 +31,69 @@ class SinglePage extends React.Component{
     }
 
     render(){
-        const page = [['VW','Golf','11','55'],['Ford','Focus','10','56']];
-        const listItems = page.map((car) => <SingleItem make ={car[0]}/>);
-        return(listItems);
+        const listItems = this.props.page.map((car) => <SingleItem key={car[0]} make ={car[0]}/>);
+        return(
+            <table className="my-table">
+                <thead>
+                    <tr className="my-tr">
+                        <th className="my-th">Make</th>
+                        <th className="my-th">Model</th>
+                        <th className="my-th">Mean Age (Years)</th>
+                        <th className="my-th">Mean Mileage (Miles)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listItems}
+                </tbody>
+            </table>
+        );
+
     }
+}
+
+class PageControl extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(<div className="PageControl">
+            Begining Previous Next End
+        </div> );
+    }
+
+}
+
+class SortControl extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(<div>
+            sort by: Mean Mileage, Mean Age, Model, Make
+        </div> );
+    }
+
+}
+
+class PagedEOLControl extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+
+        const page = [['VW','Golf','11','55'],['Ford','Focus','10','56']];
+
+        return(
+            <div>
+                <h1>Cars Taken Off Road</h1><br/>
+                <SortControl/><br/>
+                <SinglePage page={page}/>
+                <PageControl/>
+            </div>
+        );
+    }
+
 }
