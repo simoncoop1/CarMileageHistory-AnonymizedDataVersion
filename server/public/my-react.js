f0526b34e0fc607283f142ad8aa792fd2e9320c6
Simon Cooper <30930833+simoncoop1@users.noreply.github.com>
2020-09-30 22:02:44 +0100

    more functionality

--- server/public/my-react.js
+++ server/public/my-react.js
@@ -31,7 +31,7 @@ class SinglePage extends React.Component{
     }
 
     render(){
-        const listItems = this.props.page.map((car) => <SingleItem key={car['make']+car['model']} make ={car['make']} model={car['model']} avMileage="22" avAge="22"/>);
+        const listItems = this.props.page.map((car) => <SingleItem key={car['make']+car['model']} make ={car['make']} model={car['model']} avMileage={Math.round(car['mA'])} avAge={Number(car['mM'].toPrecision(3))}/>);
         return(
             <table className="my-table">
                 <thead>
@@ -54,11 +54,17 @@ class SinglePage extends React.Component{
 class PageControl extends React.Component{
     constructor(props){
         super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e){
+        this.props.onPageChange(this.props.currentPage+1);
     }
 
     render(){
+
         return(<div className="PageControl">
-            Begining Previous Next End
+            Begining Previous Next End <a href='#' onClick={this.handleClick}>next</a>
         </div> );
     }
 
@@ -67,12 +73,39 @@ class PageControl extends React.Component{
 class SortControl extends React.Component{
     constructor(props){
         super(props);
+        this.handleSortMileage = this.handleSortMileage.bind(this);
+        this.handleSortAge = this.handleSortAge.bind(this);
+        this.handleSortModel = this.handleSortModel.bind(this);
+        this.handleSortMake = this.handleSortMake.bind(this);
+    }
+
+    handleSortMileage(e){
+        e.preventDefault();
+        this.props.onSortChange("mileage");
+    }
+
+    handleSortAge(e){
+        e.preventDefault();
+        this.props.onSortChange("age");
+    }
+    handleSortMake(e){
+        e.preventDefault();
+        this.props.onSortChange("make");
+    }
+    handleSortModel(e){
+        e.preventDefault();
+        this.props.onSortChange("model");
     }
 
     render(){
+
         return(<div>
-            sort by: Mean Mileage, Mean Age, Model, Make
-        </div> );
+            sort by: 
+            <a className={this.props.sort == 'mileage'?"highlight-sort":""} href='#' onClick={this.handleSortMileage}>Mean Mileage</a>, 
+            <a className={this.props.sort == "age" ? "highlight-sort":""} href='#' onClick={this.handleSortAge}>Mean Age</a>,
+            <a className={this.props.sort == "model" ? "highlight-sort":""} href='#' onClick={this.handleSortModel}>Model</a>, 
+            <a className={this.props.sort == "make"?"highlight-sort":""} href='#' onClick={this.handleSortMake}>Make</a>
+            </div> );
     }
 
 }
@@ -80,13 +113,16 @@ class SortControl extends React.Component{
 class PagedEOLControl extends React.Component{
     constructor(props){
         super(props);
+        this.handlePageChange = this.handlePageChange.bind(this);
+        this.handleSortChange = this.handleSortChange.bind(this);
         this.state = {
             error: null,
             isLoaded:false,
             items:[],
-            page:1,
+            page:0,
             itemsPerPage:40,
-        };
+            sortType:'mileage',
+        };        
     }
 
     componentDidMount(){
@@ -100,7 +136,7 @@ class PagedEOLControl extends React.Component{
                     });
                     console.log(result.length);
                     
-                },
+                }, 
                 // Note: it's important to handle errors here
                 // instead of a catch() block so that we don't swallow
                 // exceptions from actual bugs in components.
@@ -111,26 +147,54 @@ class PagedEOLControl extends React.Component{
                     });
                 }
             )
-
     }
 
-    render(){
-        //const page = [['VW','Golf','11','55'],['Ford','Focus','10','56']];
+    handlePageChange(page){
+        this.setState({page:page});
+    }
 
-        const page = this.state.items.slice(0,40);
-
-
+    handleSortChange(sort){
+        this.setState({sortType:sort});
         
+        
+        if(sort == 'age'){
+
+            this.state.items.sort(function(a, b) {
+                return parseFloat(a.mA) - parseFloat(b.mA);
+            });
+        }
+        else if(sort == 'mileage'){
+            this.state.items.sort(function(a, b) {
+                return parseFloat(a.mM) - parseFloat(b.mM);
+            });
+        }
+        else if(sort == 'model'){
+            this.state.items.sort(function(a, b) {
+                return a.model.localeCompare(b.model);
+            });
+        }
+        else if(sort == 'make'){
+            this.state.items.sort(function(a, b) {
+                return a.make.localeCompare(b.make);
+            });
+        }
+    }
+
+    render(){ 
+        const si = this.state.page*40;
+        const ei = si + 40;
+        const page = this.state.items.slice(si,ei);
+
+        const pageCount = Math.ceil(this.state.items.length / this.state.itemsPerPage);
 
         return(
             <div className ="my-style">
                 <h1>Cars Taken Off Road</h1><br/>
-                {String(this.state.isLoaded)}<br/>
-                <SortControl/><br/>
+                  <SortControl sort={this.state.sortType} onSortChange={this.handleSortChange} /><br/>
                 <SinglePage page={page}/>
-                <PageControl/>
+                <PageControl pageCount={pageCount} currentPage={this.state.page} onPageChange={this.handlePageChange}/>
             </div>
         );
     }
 
-}
+}  
