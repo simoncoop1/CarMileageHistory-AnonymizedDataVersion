e80fd085838e58e0475a4d5f79a77734a3195213
Simon Cooper <30930833+simoncoop1@users.noreply.github.com>
2020-10-02 03:02:22 +0100

    search

--- server/public/my-react.js
+++ server/public/my-react.js
@@ -7,7 +7,6 @@ class MyComponent extends React.Component{
     }
 }
 
-
 class SingleItem extends React.Component{
     constructor(props){
         super(props);
@@ -47,7 +46,6 @@ class SinglePage extends React.Component{
                 </tbody>
             </table>
         );
-
     }
 }
 
@@ -58,16 +56,49 @@ class PageControl extends React.Component{
     }
 
     handleClick(e){
+        console.log(e.target.text);
         this.props.onPageChange(this.props.currentPage+1);
+
+        switch(e.target.text){
+            case 'Begining':
+                this.props.onPageChange(0);
+                break;
+            case 'Previous':
+                this.props.onPageChange(this.props.currentPage-1);
+                break;
+            case 'Next':
+                this.props.onPageChange(this.props.currentPage+1);
+                break;
+            case 'End':
+                this.props.onPageChange(this.props.pageCount-1);
+                break;
+        }
     }
 
     render(){
-
         return(<div className="PageControl">
-            Begining Previous Next End <a href='#' onClick={this.handleClick}>next</a>
+            <a href='#' onClick={this.handleClick}>Begining</a> <a href='#' onClick={this.handleClick}>Previous</a> <a href='#' onClick={this.handleClick}>Next</a> <a href='#' onClick={this.handleClick}>End</a> {this.props.currentPage+1}/{this.props.pageCount}
         </div> );
     }
+}
 
+class SearchControl extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleFilterTextChange =  this.handleFilterTextChange.bind(this);
+    }
+
+    handleFilterTextChange(e){
+        this.props.onFilterChange(e.target.value);
+
+    }
+
+    render(){
+        return(<div className="inputSearch">
+            <input type="text" id="search" name="search" placeholder="search.." value={this.props.filterText}
+                      onChange={this.handleFilterTextChange}/>
+        </div> );
+    }
 }
 
 class SortControl extends React.Component{
@@ -88,6 +119,7 @@ class SortControl extends React.Component{
         e.preventDefault();
         this.props.onSortChange("age");
     }
+
     handleSortMake(e){
         e.preventDefault();
         this.props.onSortChange("make");
@@ -98,7 +130,6 @@ class SortControl extends React.Component{
     }
 
     render(){
-
         return(<div>
             sort by: 
             <a className={this.props.sort == 'mileage'?"highlight-sort":""} href='#' onClick={this.handleSortMileage}>Mean Mileage</a>, 
@@ -115,6 +146,7 @@ class PagedEOLControl extends React.Component{
         super(props);
         this.handlePageChange = this.handlePageChange.bind(this);
         this.handleSortChange = this.handleSortChange.bind(this);
+        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
         this.state = {
             error: null,
             isLoaded:false,
@@ -122,6 +154,7 @@ class PagedEOLControl extends React.Component{
             page:0,
             itemsPerPage:40,
             sortType:'mileage',
+            filterText:"",
         };        
     }
 
@@ -156,9 +189,7 @@ class PagedEOLControl extends React.Component{
     handleSortChange(sort){
         this.setState({sortType:sort});
         
-        
         if(sort == 'age'){
-
             this.state.items.sort(function(a, b) {
                 return parseFloat(a.mA) - parseFloat(b.mA);
             });
@@ -180,17 +211,38 @@ class PagedEOLControl extends React.Component{
         }
     }
 
-    render(){ 
+    handleFilterTextChange(text){
+        this.setState({filterText:text,page:0})
+    }
+
+    render(){
+
+        var rows = [];
+
+        if(this.state.filterText == ""){   
+            rows = this.state.items;
+        }
+        else{
+            this.state.items.forEach((car) => {
+                if(car.model.toUpperCase().includes(this.state.filterText.toUpperCase()) ||
+                    car.make.toUpperCase().includes(this.state.filterText.toUpperCase())){
+                    rows.push(car);
+                }
+            });
+        }
+
+        const pageCount = Math.ceil(rows.length /  this.state.itemsPerPage);
         const si = this.state.page*40;
         const ei = si + 40;
-        const page = this.state.items.slice(si,ei);
-
-        const pageCount = Math.ceil(this.state.items.length / this.state.itemsPerPage);
+        const page = rows.slice(si,ei);
 
         return(
             <div className ="my-style">
                 <h1>Cars Taken Off Road</h1><br/>
-                  <SortControl sort={this.state.sortType} onSortChange={this.handleSortChange} /><br/>
+                <div className="flex-container">
+                    <SortControl sort={this.state.sortType} onSortChange={this.handleSortChange} />
+                    <SearchControl filterText={this.state.filterText} onFilterChange={this.handleFilterTextChange}/>
+                </div><br/>
                 <SinglePage page={page}/>
                 <PageControl pageCount={pageCount} currentPage={this.state.page} onPageChange={this.handlePageChange}/>
             </div>
