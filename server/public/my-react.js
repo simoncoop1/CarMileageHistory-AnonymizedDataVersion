f4c1c28e3008ac1ff8d7ce63fb0c22536b338179
Simon Cooper <30930833+simoncoop1@users.noreply.github.com>
2020-10-03 02:21:36 +0100

    fordward and back

--- server/public/my-react.js
+++ server/public/my-react.js
@@ -56,20 +56,20 @@ class PageControl extends React.Component{
     }
 
     handleClick(e){
-        console.log(e.target.text);
-        this.props.onPageChange(this.props.currentPage+1);
+        //console.log(e);
+        //this.props.onPageChange(this.props.currentPage+1);
 
-        switch(e.target.text){
-            case 'Begining':
+        switch(e.currentTarget['id']){
+            case 'begining':
                 this.props.onPageChange(0);
                 break;
-            case 'Previous':
+            case 'previous':
                 this.props.onPageChange(this.props.currentPage-1);
                 break;
-            case 'Next':
+            case 'next':
                 this.props.onPageChange(this.props.currentPage+1);
                 break;
-            case 'End':
+            case 'end':
                 this.props.onPageChange(this.props.pageCount-1);
                 break;
         }
@@ -77,7 +77,21 @@ class PageControl extends React.Component{
 
     render(){
         return(<div className="PageControl">
-            <a href='#' onClick={this.handleClick}>Begining</a> <a href='#' onClick={this.handleClick}>Previous</a> <a href='#' onClick={this.handleClick}>Next</a> <a href='#' onClick={this.handleClick}>End</a> {this.props.currentPage+1}/{this.props.pageCount}
+            <button id="begining" className="my-button" onClick={this.handleClick}><svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-chevron-double-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+              <path fillRule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+            </svg></button>
+            <button id="previous" className="my-button" onClick={this.handleClick}><svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-chevron-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+            </svg></button>
+            <button id="next" className="my-button" onClick={this.handleClick}><svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+            </svg></button>
+            <button id="end" className="my-button" onClick={this.handleClick}><svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-chevron-double-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"/>
+              <path fillRule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"/>
+            </svg></button>
+            {this.props.currentPage+1}/{this.props.pageCount} 
         </div> );
     }
 }
